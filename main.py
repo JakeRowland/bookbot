3fe051119c55691730828f65a56aa291ef685da8
JakeRowland <jake.rowland1989@gmail.com>
2024-04-24 23:29:56 -0700

    Created sort letters funtion and added reporting to main function

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ def main():
         file_contents=f.read()
         #add code here if you want to process or print file_contents
         number_of_words=wordcount(file_contents)
+        letters=lettercount(file_contents)
+        sorted_letters=sort_letters(letters)
+        for letter, count in sorted_letters:
+            print (f"the {letter} appears {count} times")
     print (f"{number_of_words} words found in document")
+    
 
 #This splits file_contents into words and the counts/outputs the number of words
 def wordcount(file_contents):
@@ -17,10 +22,22 @@ def lettercount(file_contents):
     lower_case=file_contents.lower()
     for letter in lower_case:
         if letter in letters_dict:
-            letters_dict[letter]=letters_dict[letter]+1
+            letters_dict[letter]+=1
         else:
             letters_dict[letter]=1
     return letters_dict
+
+#This sorts the results from the lettercount function into a list for reporting
+def sort_letters(letters):
+    report=[]
+    for letter in letters:
+        if letter.isalpha():
+            report.append((letter, letters[letter]))
+    report.sort(key=lambda x: x[1], reverse=True)
+    return report
+    
+
+
 
 
 #This calls the main function
